913c76dba641391ee44845dddf1b3cfa34a3d301
kishimoto-banana <drehbleistift@gmail.com>
2019-03-24 00:09:40 +0900

    Mod setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,39 @@
 from setuptools import setup, find_packages
 import os
 
+
 def strip_comments(l):
     return l.split('#', 1)[0].strip()
 
+
 def reqs(*f):
-    return list(filter(None, [strip_comments(l) for l in open(os.path.join(os.getcwd(), *f)).readlines()]))
+    return list(
+        filter(None, [
+            strip_comments(l)
+            for l in open(os.path.join(os.getcwd(), *f)).readlines()
+        ]))
+
+
+def read_file(filename):
+    basepath = os.path.dirname(os.path.dirname((__file__)))
+    filepath = os.path.join(basepath, filename)
+    if os.path.exists(filepath):
+        return open(filepath).read()
+    else:
+        return ''
+
 
 setup(
     name='velvet',
-    version='0.0.2',
+    version='0.0.1',
     description='カテゴリ変数からの特徴抽出',
-    long_description='カテゴリ変数からの特徴抽出方法をまとめたパッケージです',
+    long_description=read_file('README.md'),
     author='Masashi Kishimoto',
-    license='MIT',
+    author_email='drehbleistift@gmail.com',
+    url='https://github.com/kishimoto-banana/velvet',
     classifiers=[
-        'Development Status :: 1 - Planning'
+        'Development Status :: 4 - Beta', 'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6'
     ],
     install_requires=reqs('requirements.txt'),
-    packages=find_packages(exclude=('tests', 'docs'))
-)
+    packages=find_packages(exclude=('tests', 'docs')))
